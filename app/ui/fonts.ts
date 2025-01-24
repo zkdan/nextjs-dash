caf15d61e5dc690a9a92509c0ddf0a47f7f43de6
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2025-01-24 11:26:15 -0500

    f

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter, Ruslan_Display, Lusitana} from 'next/font/google';
+
+export const inter = Inter({subsets:['latin']})
+
+export const lusitana = Lusitana({subsets:['latin'], weight:['400', '700']})
+
+export const ruslan_display = Ruslan_Display({subsets:['latin'], weight:"400"})
